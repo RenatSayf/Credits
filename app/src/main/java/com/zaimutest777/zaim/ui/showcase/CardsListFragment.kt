5421b174562d250bfee28c7bb37d3851c39357b7
renatsayf <renatsayf@gmail.com>
2021-07-11 13:56:47 +0500

    cards_list_fragment.xml has been fixed

--- app/src/main/java/com/zaimutest777/zaim/ui/showcase/CardsListFragment.kt
+++ app/src/main/java/com/zaimutest777/zaim/ui/showcase/CardsListFragment.kt
@@ -1,24 +1,31 @@
 package com.zaimutest777.zaim.ui.showcase
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
 import com.zaimutest777.zaim.MyInitialActivity
 import com.zaimutest777.zaim.R
-import com.zaimutest777.zaim.databinding.CardsListFragmentBinding
+import com.zaimutest777.zaim.ui.adapters.ProductAdapter
+import com.zaimutest777.zaim.utils.NetworkState
+import com.zaimutest777.zaim.viewmodels.DbJsonViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class CardsListFragment : Fragment(R.layout.cards_list_fragment)
+class CardsListFragment : Fragment()
 {
-    //private lateinit var binding: CardsListFragmentBinding
     private lateinit var mActivity: MyInitialActivity
+    private lateinit var dbJsonVM: DbJsonViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -48,7 +55,85 @@ class CardsListFragment : Fragment(R.layout.cards_list_fragment)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
-        //binding = CardsListFragmentBinding.bind(view)
+        dbJsonVM = ViewModelProvider(this)[DbJsonViewModel::class.java]
+
+        val cardsRecyclerView = view.findViewById<RecyclerView>(R.id.cardsRecyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(mActivity)
+        }
+
+        val tabLayoutView = view.findViewById<TabLayout>(R.id.tabLayoutView)
+        tabLayoutView.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
+        {
+            override fun onTabSelected(tab: TabLayout.Tab?)
+            {
+                when(tab!!.text)
+                {
+                    mActivity.getString(R.string.text_credit) ->
+                    {
+                        val list = dbJsonVM.creditCards.value
+                        list?.let {
+                            val productAdapter = ProductAdapter(it).apply {
+                                stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+                            }
+                            cardsRecyclerView.adapter = productAdapter
+                        }
+                    }
+                    mActivity.getString(R.string.text_debit) ->
+                    {
+                        val list = dbJsonVM.debitCards.value
+                        list?.let {
+                            val productAdapter = ProductAdapter(it).apply {
+                                stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+                            }
+                            cardsRecyclerView.adapter = productAdapter
+                        }
+                    }
+                    mActivity.getString(R.string.text_rassrochka) ->
+                    {
+                        val list = dbJsonVM.rasrochka.value
+                        list?.let {
+                            val productAdapter = ProductAdapter(it).apply {
+                                stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+                            }
+                            cardsRecyclerView.adapter = productAdapter
+                        }
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?){}
+
+            override fun onTabReselected(tab: TabLayout.Tab?){}
+        })
+
+        dbJsonVM.creditCards.observe(viewLifecycleOwner, { list ->
+            list?.let {
+                val productAdapter = ProductAdapter(it).apply {
+                    stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+                }
+                cardsRecyclerView.adapter = productAdapter
+            }
+        })
+
+        val loadProgBar = view.findViewById<ProgressBar>(R.id.loadProgBar)
+        dbJsonVM.netState.observe(viewLifecycleOwner, { state ->
+            when(state)
+            {
+                is NetworkState.Completed ->
+                {
+                    loadProgBar.visibility = View.INVISIBLE
+                }
+                is NetworkState.Error ->
+                {
+                    loadProgBar.visibility = View.INVISIBLE
+                }
+                is NetworkState.Loading ->
+                {
+                    loadProgBar.visibility = View.VISIBLE
+                }
+            }
+        })
 
 
     }
